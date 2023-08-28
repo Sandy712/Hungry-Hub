f5de8f60899a339ca2d10dc379d2c1ce3fed0c7c
Sandeep Prajapati <sandeepprajapat200@gmail.com>
2023-08-28 22:08:51 +0530

    refactor cart use mui

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import { ListGroupItem } from "react-bootstrap";
-import {IoAddCircleSharp} from "react-icons/io5";
-import {AiFillMinusCircle} from "react-icons/ai";
-import {GiCrossMark} from "react-icons/gi";
+import React from 'react';
+import { Paper, Typography, IconButton, Divider, Stack } from '@mui/material';
+import { IoAddCircleSharp } from 'react-icons/io5';
+import { AiFillMinusCircle } from 'react-icons/ai';
+// import { GiCrossMark } from 'react-icons/gi';
+import CloseIcon from '@mui/icons-material/Close';
 
-import "./cart-item.css";
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cartSlice';
 
-import { useDispatch } from "react-redux";
-import { cartActions } from "../../store/cartSlice";
+import './cart-item.css';
 
 const CartItem = ({ item }) => {
   const { id, title, price, image, quantity, totalPrice } = item;
@@ -34,33 +35,36 @@ const CartItem = ({ item }) => {
   };
 
   return (
-    <ListGroupItem className="border-0 cart__item">
+    <Paper elevation={4} className="cart__item">
       <div className="cart__item-info d-flex gap-2">
         <img src={image} alt="product-img" />
 
-        <div className="cart__product-info w-100 d-flex align-items-center gap-4 justify-content-between">
+        <div className="cart__product-info w-100 d-flex align-items-center  justify-content-between">
           <div>
-            <h6 className="cart__product-title">{title}</h6>
-            <p className=" d-flex align-items-center gap-5 cart__product-price">
+            <Typography variant="h6" className="cart__product-title">
+              {title}
+            </Typography>
+            <Typography className="d-flex align-items-center gap-5 cart__product-price">
               {quantity}x <span>${totalPrice}</span>
-            </p>
-            <div className=" d-flex align-items-center justify-content-between increase__decrease-btn">
-              <span className="increase__btn" onClick={incrementItem}>
-              <IoAddCircleSharp/>
-              </span>
-              <span className="quantity">{quantity}</span>
-              <span className="decrease__btn" onClick={decreaseItem}>
-                <AiFillMinusCircle/>
-              </span>
-            </div>
+            </Typography>
+            <Stack direction="row" spacing={1} className="increase__decrease-btn" style={{width:"110px"}}>
+              <IconButton className="decrease__btn" onClick={decreaseItem}>
+                <AiFillMinusCircle />
+              </IconButton>
+              <Typography className="quantity" style={{display:"flex",justifyContent:"center",alignItems:"center"}}>{quantity}</Typography>
+              <IconButton className="increase__btn" onClick={incrementItem}>
+                <IoAddCircleSharp />
+              </IconButton>
+            </Stack>
           </div>
 
-          <span className="delete__btn" onClick={deleteItem}>
-            <GiCrossMark/>
-          </span>
+          <IconButton className="delete__btn" onClick={deleteItem}>
+            <CloseIcon style={{ fontSize:"30px", color: 'black' }}/>
+          </IconButton>
         </div>
       </div>
-    </ListGroupItem>
+      <Divider />
+    </Paper>
   );
 };
 
--- src/components/Cart/Carts.jsx
+++ src/components/Cart/Carts.jsx
@@ -1,14 +1,12 @@
-import React from "react";
+import React from 'react';
+import { List, Typography, IconButton, Paper } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import ClearSharpIcon from '@mui/icons-material/ClearSharp';
+import { cartUiActions } from '../../store/cartUiSlice';
 
-import { ListGroup } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import CartItem from "./CartItem";
-import {ImCross} from "react-icons/im";
-
-import { useDispatch, useSelector } from "react-redux";
-import { cartUiActions } from "../../store/cartUiSlice";
-
-import "./shopping-cart.css";
+import CartItem from './CartItem';
+import './shopping-cart.css';
 
 const Carts = () => {
   const dispatch = useDispatch();
@@ -18,36 +16,39 @@ const Carts = () => {
   const toggleCart = () => {
     dispatch(cartUiActions.toggle());
   };
+
   return (
     <div className="cart__container">
-      <ListGroup className="cart">
+      <Paper className="cart">
         <div className="cart__close">
-          <span onClick={toggleCart}>
-            <ImCross/>
-          </span>
+          <IconButton onClick={toggleCart}>
+            <ClearSharpIcon style={{ fontSize:"40px", color: '#0f172a' }} />
+          </IconButton>
         </div>
 
-        <div className="cart__item-list">
+        <List className="cart__item-list">
           {cartProducts.length === 0 ? (
-            <h6 className="text-center mt-5">No item added to the cart</h6>
+            <Typography variant="h6" align="center" mt={5}>
+              No item added to the cart
+            </Typography>
           ) : (
             cartProducts.map((item, index) => (
               <CartItem item={item} key={index} />
             ))
           )}
-        </div>
+        </List>
 
         <div className="cart__bottom d-flex align-items-center justify-content-between">
-          <h6>
+          <Typography>
             Subtotal : <span>${totalAmount}</span>
-          </h6>
+          </Typography>
           <button>
             <Link to="/checkout" onClick={toggleCart}>
               Checkout
             </Link>
           </button>
         </div>
-      </ListGroup>
+      </Paper>
     </div>
   );
 };
