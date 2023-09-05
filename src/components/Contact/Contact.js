a9ecb5e7155c15275a3e61889a0fa7af79bb8fdc
Sandeep Prajapati <sandeepprajapat200@gmail.com>
2023-09-05 12:19:29 +0530

    feat firebase database

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaPhone, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 import "./contact.css"
+import { getDatabase, ref, push } from 'firebase/database';
+
+
 
 const Contact = () => {
+
+    const [userData, setuserData] = useState(
+        {
+            Full_name: "",
+            Email: "",
+            Phone_no: "",
+            Message: "",
+        }
+    );
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setuserData({
+            ...userData,
+            [name]: value,
+        });
+    };
+
+
+    const postuserData = () => {
+        const database = getDatabase();
+        const userRef = ref(database, 'UserData'); // Replace 'users' with your desired database path
+
+        // Push the user data to the database
+        push(userRef, userData);
+        
+        alert("Send Successfully...");
+        // clear the form fields after data is submitted
+        setuserData({
+            Full_name: '',
+            Email: '',
+            Phone_no: '',
+            Message: '',
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        postuserData();
+    };
+
+
+
+
     return (
         <section className="container">
             <header className="header">
@@ -29,22 +76,22 @@ const Contact = () => {
                 </article>
                 <article className="right-main">
                     {/* <h1 style={{ marginBottom: "25px" }}>Contact Us</h1> */}
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <article className="form">
                             <label htmlFor="fullName">Full Name</label>
-                            <input id="fullName" type="text" name="fullName" placeholder="Enter Your Full Name" />
+                            <input id="fullName" type="text" name="Full_name" placeholder="Enter Your Full Name" value={userData.Full_name} onChange={handleChange} />
                         </article>
                         <article className="form">
                             <label htmlFor="email">Email</label>
-                            <input id="email" type="email" name="email" placeholder="Enter Your Email Address" />
+                            <input id="email" type="email" name="Email" placeholder="Enter Your Email Address" value={userData.Email} onChange={handleChange} />
                         </article>
                         <article className="form">
                             <label htmlFor="phone">Phone Number</label>
-                            <input id="phone" type="tel" name="phone" placeholder="Enter Your Phone Number" />
+                            <input id="phone" type="tel" name="Phone_no" placeholder="Enter Your Phone Number" value={userData.Phone_no} onChange={handleChange} />
                         </article>
                         <article className="form">
                             <label htmlFor="message">Message</label>
-                            <textarea id="message" type="text" placeholder="Write Your Message"></textarea>
+                            <textarea id="message" type="text" name="Message" placeholder="Write Your Message" value={userData.Message} onChange={handleChange}></textarea>
                         </article>
                         <article>
                             <button className="done" type="submit">Send Message</button>
